fd821fd79ca39f59bd6dfb0682c1c3f0e4cc5267
Julio Mario <jtuburon@gmail.com>
2015-10-22 14:02:11 -0500

    tag cloud added with sample data

--- static/dist/js/main.js
+++ static/dist/js/main.js
@@ -38,8 +38,9 @@ function run_queries(){
 	    error: function (request, status, err) {
 	    }
 	});
-}
 
+	initTagsCloud();
+}
 
 function draw_folllowers_plot(data){
 	$("#chart").html('');
@@ -159,6 +160,43 @@ function draw_folllowers_plot(data){
 		}
 	}
 	
+}
+
+
+function initTagsCloud(){
+  var fill = d3.scale.category20();
+
+  d3.layout.cloud().size([300, 300])
+      .words([
+        ".NET", "Silverlight", "jQuery", "CSS3", "HTML5", "JavaScript", "SQL","C#"].map(function(d) {
+        return {text: d, size: 10 + Math.random() * 50};
+      }))
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
+
+  function draw(words) {
+    d3.select("#cloud-div").append("svg")
+        .attr("width", 500)
+        .attr("height", 500)
+      .append("g")
+        .attr("transform", "translate(150,150)")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  }
+
+  alert("HOLAA");
 }
 
 $(document).ready( function() {
